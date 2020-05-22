2d6dd9546accc3d6a2fe99c9a272e60a72a96223
MK-D-Luffy <1905470291@qq.com>
2020-05-22 23:07:24 +0800

    第一个MyBatis程序

---
+++ MyBatis-01/src/main/java/com/study/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.study.dao;
+
+
+import com.study.pojo.User;
+
+import java.util.List;
+
+/**
+ * 今天也要元气满满啊！
+ * 今天也要加油啊！
+ * 道路,很远!脚步,更长!
+ */
+public interface UserDao {
+    List<User> getUserList();
+}
